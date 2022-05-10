d075101a78d7aae554d1bd1d8b9d17ad03c796ad
Kalana99 <kalanarajika99@gmail.com>
2022-05-10 12:00:54 +0530

    ApplicationTest implemented

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,87 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
+import static org.junit.Assert.assertTrue;
+
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.manipulation.Ordering;
+
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentDemoExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DBHelper;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest {
+
+    private static DBHelper dbHelper;
+    private static PersistentDemoExpenseManager expenseManager;
+
+    @BeforeClass
+    public static void start(){
+        Context context = ApplicationProvider.getApplicationContext();
+        dbHelper = new DBHelper(context);
+        try {
+            expenseManager = new PersistentDemoExpenseManager(dbHelper);
+        } catch (ExpenseManagerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddAccount(){
+
+        expenseManager.addAccount("22AABB", "PB", "Kalana", 1000.0);
+
+        List<String> acc_no_list = expenseManager.getAccountNumbersList();
+        assertTrue(acc_no_list.contains("22AABB"));
+    }
+
+    @Test
+    public void testIncomeTransaction() throws InvalidAccountException {
+
+        expenseManager.addAccount("22AABB", "PB", "Kalana", 1000.0);
+        expenseManager.updateAccountBalance("22AABB", 22, 5, 2022, ExpenseType.INCOME, "2000.0");
+
+        List<Transaction> transactions = expenseManager.getTransactionLogs();
+        Transaction last = transactions.get(transactions.size() - 1);
+        String acc_no = last.getAccountNo();
+        Account acc = dbHelper.getAccount(acc_no);
+
+        assertTrue(acc_no, acc_no.equals("22AABB"));
+        assertTrue(String.valueOf(last.getAmount()), last.getAmount() == 2000.0);
+        assertTrue(String.valueOf(acc.getBalance()), acc.getBalance() == 3000.0);
+    }
+
+    @Test
+    public void testExpenseTransaction() throws InvalidAccountException {
+
+        expenseManager.updateAccountBalance("22AABB", 22, 5, 2022, ExpenseType.EXPENSE, "250.0");
+
+        List<Transaction> transactions = expenseManager.getTransactionLogs();
+        Transaction last = transactions.get(transactions.size() - 1);
+        String acc_no = last.getAccountNo();
+        Account acc = dbHelper.getAccount(acc_no);
+
+        assertTrue(acc_no.equals("22AABB"));
+        assertTrue(String.valueOf(last.getAmount()), last.getAmount() == 250.0);
+        assertTrue(String.valueOf(acc.getBalance()),acc.getBalance() == 2750.0);
     }
 }
